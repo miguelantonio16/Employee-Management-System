d3b6bc1e6717540de3cd6e0c4adbcc42b5e9eabe
Miguel Antonio Barcala <ma.barcala16@gmail.com>
2026-01-16 21:20:35 +0800

    Project files

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace EMS_BARCALA.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Head { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EMS_BARCALA.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Department { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
